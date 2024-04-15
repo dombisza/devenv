9b3f01a76deca9181dc4031fcb7911353d9836ba
dombisza <dombisza@gmail.com>
2024-04-15 08:42:15 +0200

    observability terraform files

---
+++ terraform/core/kube_infra/observability/settings.tf
@@ -0,0 +1,18 @@
+# This file has been generated from ./tf_backend/main.tf:null_resource
+terraform {
+  required_version = "<=1.5.5, >=v1.4.6"
+  backend "s3" {
+    endpoint                    = "obs.eu-de.otc.t-systems.com"
+    bucket                      = "sdombi-dev-tfstate"
+    key                         = "personal/obs-services"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    region                      = "eu-de"
+   }
+   required_providers {
+     opentelekomcloud = {
+     source  = "opentelekomcloud/opentelekomcloud"
+     version = ">=1.35.6"
+     }
+   }
+}
