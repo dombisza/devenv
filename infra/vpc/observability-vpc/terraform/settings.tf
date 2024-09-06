f56e405527ef013cb3f742a2c5ce092080afda88
dombisza <dombisza@gmail.com>
2024-09-06 08:55:57 +0200

    wip

--- infra/vpc/observability-vpc/terraform/settings.tf
+++ infra/vpc/observability-vpc/terraform/settings.tf
@@ -2,12 +2,12 @@
 terraform {
   required_version = "<=1.5.5, >=v1.4.6"
   backend "s3" {
-    endpoint                    = "obs.eu-nl.otc.t-systems.com"
-    bucket                      = "sdombi-rnd-state"
-    key                         = "personal/obs-services-vpc"
+    endpoint                    = "obs.eu-de.otc.t-systems.com"
+    bucket                      = "sdombi-dev-tfstate"
+    key                         = "personal/obs-services-vpcdev"
     skip_region_validation      = true
     skip_credentials_validation = true
-    region                      = "eu-nl"
+    region                      = "eu-de"
    }
    required_providers {
      opentelekomcloud = {
